f0ad70a9cb3a17f5770de0b1c432f6c6bdf5c894
root <root@DESKTOP-U4P7FN4.localdomain>
2024-08-01 17:58:29 +0300

    fix indent

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import List, Union, Tuple, Callable, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns list."""
     return [(i, len(i)) for i in lst]
