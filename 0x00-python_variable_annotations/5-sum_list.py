63363f4b6dc5137981b607705d8b1a938d2a0e9e
root <root@DESKTOP-U4P7FN4.localdomain>
2024-08-01 16:28:05 +0300

    try fix this

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
 """Type-annotated function sum_list."""
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum as a float."""
-    sum: float = 0
-    for l in input_list:
-        sum += l
-    return float(sum)
+    return sum(input_list)
