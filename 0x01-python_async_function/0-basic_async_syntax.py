8be2785d19d3ef0cb8b808356ae99458768258e4
root <root@DESKTOP-U4P7FN4.localdomain>
2024-08-05 18:32:48 +0300

    fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,4 +7,5 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """Waits for a random delay between 0 and max_delay."""
     delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
